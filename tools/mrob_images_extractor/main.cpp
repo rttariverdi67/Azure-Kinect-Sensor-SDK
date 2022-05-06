21c97dd0713a06cf7f616175bc31de8ea4d0b7ca
Marselka <ma.faizullin@gmail.com>
2022-05-06 10:47:58 +0300

    fully working paral-ed images extractor

--- tools/mrob_images_extractor/main.cpp
+++ tools/mrob_images_extractor/main.cpp
@@ -13,6 +13,144 @@
 
 #include <chrono>
 
+#include <algorithm>
+#include <thread>
+#include <functional>
+#include <vector>
+
+#define MAX_NUMBER_OF_CAPTURES 259200
+#define CAPTURES_IN_BATCH 256
+
+//From https://stackoverflow.com/questions/36246300/parallel-loops-in-c
+/// @param[in] nb_elements : size of your for loop
+/// @param[in] functor(start, end) :
+/// your function processing a sub chunk of the for loop.
+/// "start" is the first index to process (included) until the index "end"
+/// (excluded)
+/// @code
+///     for(int i = start; i < end; ++i)
+///         computation(i);
+/// @endcode
+/// @param use_threads : enable / disable threads.
+///
+///
+static
+void parallel_for(unsigned nb_elements,
+                  std::function<void (int start, int end)> functor,
+                  bool use_threads = true)
+{
+    // -------
+    unsigned nb_threads_hint = std::thread::hardware_concurrency();
+    unsigned nb_threads = nb_threads_hint == 0 ? 8 : (nb_threads_hint);
+
+    unsigned batch_size = nb_elements / nb_threads;
+    unsigned batch_remainder = nb_elements % nb_threads;
+
+    std::vector< std::thread > my_threads(nb_threads);
+
+    if( use_threads )
+    {
+        // Multithread execution
+        for(unsigned i = 0; i < nb_threads; ++i)
+        {
+            int start = i * batch_size;
+            my_threads[i] = std::thread(functor, start, start+batch_size);
+        }
+    }
+    else
+    {
+        // Single thread execution (for easy debugging)
+        for(unsigned i = 0; i < nb_threads; ++i){
+            int start = i * batch_size;
+            functor( start, start+batch_size );
+        }
+    }
+
+    // Deform the elements left
+    int start = nb_threads * batch_size;
+    functor( start, start+batch_remainder);
+
+    // Wait for the other thread to finish their task
+    if( use_threads )
+        std::for_each(my_threads.begin(), my_threads.end(), std::mem_fn(&std::thread::join));
+}
+
+//extract(playback, &capture, output_path, transformation, transformed_depth_image, color_image_width_pixels, color_image_height_pixels, distortion, matrix, compression_params);
+
+bool extract(k4a_playback_t playback_handle, k4a_capture_t capture, const char * output_path, k4a_transformation_t transformation, k4a_image_t transformed_depth_image,
+    int color_image_width_pixels, int color_image_height_pixels, cv::Mat distortion, cv::Matx33d matrix, std::vector<int> compression_params)
+{
+    
+    k4a_image_t depth_image = NULL;
+    k4a_image_t color_image = NULL;
+
+    uint64_t color_timestamp;
+    uint64_t depth_timestamp;
+    char color_filename [1024];
+    char depth_filename [1024];
+
+    uint8_t * buffer;
+    uint32_t size;
+
+    cv::Mat img_array;
+    cv::Mat img_array_undistorted;
+
+    // Fetch color and depth frames
+    color_image = k4a_capture_get_color_image(capture);
+    if (color_image == 0) {
+        k4a_capture_release(capture);
+        return true;
+    }
+
+    depth_image = k4a_capture_get_depth_image(capture);
+    if (depth_image == 0) {
+        k4a_image_release(color_image);
+        k4a_capture_release(capture);
+        return true;
+    }
+
+    color_timestamp = k4a_image_get_device_timestamp_usec(color_image);
+    sprintf (color_filename, "%s/color/%012ld.jpg", output_path, color_timestamp);
+    depth_timestamp = k4a_image_get_device_timestamp_usec(depth_image);
+    sprintf (depth_filename, "%s/depth/%012ld.png", output_path, depth_timestamp);
+
+    if (K4A_RESULT_SUCCEEDED !=
+        k4a_transformation_depth_image_to_color_camera(transformation, depth_image, transformed_depth_image))
+    {
+        printf("Failed to compute transformed depth image\n");
+        return false;
+    }
+
+    buffer = k4a_image_get_buffer(color_image);
+    size = k4a_image_get_size(color_image);
+    
+    img_array = cv::imdecode(cv::Mat(1, size, CV_8UC1, buffer), cv::IMREAD_UNCHANGED);
+    cv::undistort(img_array, img_array_undistorted, matrix, distortion);
+    cv::imwrite(color_filename, img_array_undistorted, compression_params);
+
+    buffer = k4a_image_get_buffer(transformed_depth_image);
+    size = k4a_image_get_size(transformed_depth_image);
+    
+    img_array = cv::Mat(color_image_height_pixels, color_image_width_pixels, CV_16UC1, buffer);
+    cv::undistort(img_array, img_array_undistorted, matrix, distortion);
+    cv::imwrite(depth_filename,  img_array_undistorted);
+
+    if (depth_image != NULL)
+    {
+        k4a_image_release(depth_image);
+    }
+    if (color_image != NULL)
+    {
+        k4a_image_release(color_image);
+    }
+    if (capture != NULL)
+    {
+        k4a_capture_release(capture);
+    }
+
+}
+
+
 static int playback(char *input_path, const char * output_path)
 {
     int return_code = 1;
@@ -24,23 +162,14 @@ static int playback(char *input_path, const char * output_path)
     struct k4a_calibration_intrinsic_parameters_t::_param param;
 
     k4a_capture_t capture = NULL;
-    k4a_image_t depth_image = NULL;
-    k4a_image_t color_image = NULL;
-    k4a_image_t transformed_depth_image = NULL;
+    k4a_capture_t array_of_captures[CAPTURES_IN_BATCH];
+    k4a_image_t array_of_transformed_depth_images[CAPTURES_IN_BATCH];
 
     k4a_result_t result;
     k4a_stream_result_t stream_result;
 
-    uint64_t color_timestamp;
-    uint64_t depth_timestamp;
-    char color_filename [1024];
-    char depth_filename [1024];
-    uint32_t i;
-
     int color_image_width_pixels;
     int color_image_height_pixels;
-    uint8_t * buffer;
-    uint32_t size;
 
     cv::Mat distortion;
     cv::Matx33d matrix;
@@ -51,117 +180,93 @@ static int playback(char *input_path, const char * output_path)
     compression_params.push_back(cv::IMWRITE_JPEG_QUALITY);
     compression_params.push_back(96);
 
+    uint32_t i;
+    uint32_t number_of_captures = 0;
+    bool flag_to_finish = false;
+
     // Open recording
     result = k4a_playback_open(input_path, &playback);
     if (result != K4A_RESULT_SUCCEEDED || playback == NULL)
     {
         printf("Failed to open recording %s\n", input_path);
-        return return_code;
+        return 1;
     }
 
     if (K4A_RESULT_SUCCEEDED != k4a_playback_get_calibration(playback, &calibration))
     {
         printf("Failed to get calibration\n");
-        return return_code;
+        return 1;
     }
 
     transformation = k4a_transformation_create(&calibration);
     
     calib_color = calibration.color_camera_calibration;
+    color_image_width_pixels = calib_color.resolution_width;
+    color_image_height_pixels = calib_color.resolution_height;
     param = calib_color.intrinsics.parameters.param;
     distortion = (cv::Mat_<double>(8,1) << param.k1, param.k2, param.p1, param.p2, param.k3, param.k4, param.k5, param.k6);
     matrix = cv::Matx33d(param.fx, 0.0, param.cx, 0.0, param.fy, param.cy, 0.0, 0.0, 1.0);
-
-    color_image_width_pixels = calib_color.resolution_width;
-    color_image_height_pixels = calib_color.resolution_height;
-    if (K4A_RESULT_SUCCEEDED != k4a_image_create(K4A_IMAGE_FORMAT_DEPTH16,
-                                                 color_image_width_pixels,
-                                                 color_image_height_pixels,
-                                                 color_image_width_pixels * (int)sizeof(uint16_t),
-                                                 &transformed_depth_image))
-    {
-        printf("Failed to create transformed depth image\n");
-        return return_code;
-    }
     
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-
-    for (i = 0; i < 2592000; i++) { // 24h*3600s*30fps = 2592000
-        stream_result = k4a_playback_get_next_capture(playback, &capture);
-        if (stream_result != K4A_STREAM_RESULT_SUCCEEDED || capture == NULL)
+    // Init depth frame buffer
+    for (i = 0; i < CAPTURES_IN_BATCH; i++) {
+        array_of_captures[i] = NULL;
+        if (K4A_RESULT_SUCCEEDED != k4a_image_create(K4A_IMAGE_FORMAT_DEPTH16,
+                                                     color_image_width_pixels,
+                                                     color_image_height_pixels,
+                                                     color_image_width_pixels * (int)sizeof(uint16_t),
+                                                     &array_of_transformed_depth_images[i]))
         {
-            if (stream_result == K4A_STREAM_RESULT_EOF) {
-                return_code = 0;
-            }
-            else {
-                printf("Failed to fetch frame\n");
-            }
+            printf("Failed to create transformed depth image\n");
+            return 1;
+        }
+    }
+
+    for (uint32_t batch = 0; batch < MAX_NUMBER_OF_CAPTURES / CAPTURES_IN_BATCH; batch++) {
+        if (flag_to_finish == true) {
             break;
         }
+        printf("Batch #%d, ", batch + 1);
 
-        // Fetch color and depth frames
-        color_image = k4a_capture_get_color_image(capture);
-        if (color_image == 0) {
-            k4a_capture_release(capture);
-            continue;
+        number_of_captures = 0;
+        for (i = 0; i < CAPTURES_IN_BATCH; i++) {
+            stream_result = k4a_playback_get_next_capture(playback, &array_of_captures[i]);
+            if (stream_result != K4A_STREAM_RESULT_SUCCEEDED || array_of_captures[i] == NULL)
+            {
+                if (stream_result == K4A_STREAM_RESULT_EOF) {
+                    flag_to_finish = true;
+                }
+                else {
+                    printf("Failed to fetch frame\n");
+                    return 1;
+                }
+                break;
+            }
         }
+        number_of_captures = i;
 
-        depth_image = k4a_capture_get_depth_image(capture);
-        if (depth_image == 0) {
-            k4a_image_release(color_image);
-            k4a_capture_release(capture);
-            continue;
-        }
+        printf("%d captures, ", number_of_captures);
 
-        color_timestamp = k4a_image_get_device_timestamp_usec(color_image);
-        sprintf (color_filename, "%s/color/%012ld.jpg", output_path, color_timestamp);
-        depth_timestamp = k4a_image_get_device_timestamp_usec(depth_image);
-        sprintf (depth_filename, "%s/depth/%012ld.png", output_path, depth_timestamp);
-
-        if (K4A_RESULT_SUCCEEDED !=
-            k4a_transformation_depth_image_to_color_camera(transformation, depth_image, transformed_depth_image))
-        {
-            printf("Failed to compute transformed depth image\n");
-            return return_code;
-        }
-
-        buffer = k4a_image_get_buffer(color_image);
-        size = k4a_image_get_size(color_image);
+        //std::this_thread::sleep_for(std::chrono::seconds(10));
         
-        img_array = cv::imdecode(cv::Mat(1, size, CV_8UC1, buffer), cv::IMREAD_UNCHANGED);
-        cv::undistort(img_array, img_array_undistorted, matrix, distortion);
-        cv::imwrite(color_filename, img_array_undistorted, compression_params);
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
-        buffer = k4a_image_get_buffer(transformed_depth_image);
-        size = k4a_image_get_size(transformed_depth_image);
-        
-        img_array = cv::Mat(color_image_height_pixels, color_image_width_pixels, CV_16UC1, buffer);
-        cv::undistort(img_array, img_array_undistorted, matrix, distortion);
-        cv::imwrite(depth_filename,  img_array_undistorted);
+        // The core loop
+        parallel_for(number_of_captures, [&](int start, int end){ 
+            for(int i = start; i < end; ++i)
+                extract(playback, array_of_captures[i], output_path, transformation, array_of_transformed_depth_images[i], color_image_width_pixels, color_image_height_pixels, distortion, matrix, compression_params);
+        });
 
-        if (depth_image != NULL)
-        {
-            k4a_image_release(depth_image);
-        }
-        if (color_image != NULL)
-        {
-            k4a_image_release(color_image);
-        }
-        if (capture != NULL)
-        {
-            k4a_capture_release(capture);
-        }
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        float res = static_cast< float >(number_of_captures) * 1000000.0 / static_cast< float >(std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count());
+        printf("%f fps.\n", res);
     }
 
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-
-    float res = static_cast< float >(i) * 1000000.0 / static_cast< float >(std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count());
-    printf("%f fps\n", res);
-
-
-    if (transformed_depth_image != NULL)
-    {
-        k4a_image_release(transformed_depth_image);
+    // Release
+    for (i = 0; i < CAPTURES_IN_BATCH; i++) {
+      if (array_of_transformed_depth_images[i] != NULL)
+      {
+          k4a_image_release(array_of_transformed_depth_images[i]);
+      }   
     }
     if (playback != NULL)
     {
@@ -172,7 +277,7 @@ static int playback(char *input_path, const char * output_path)
         k4a_transformation_destroy(transformation);
     }
 
-    return return_code;
+    return 0;
 }
 
 int main(int argc, char **argv)
